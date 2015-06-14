86c86313f9868c638044e83c4508487410108c2e
athulnath <athulnath.t@gmail.com>
2015-06-14 11:23:13 +0530

    added digit search and replace

--- digit_count_replace.php
+++ digit_count_replace.php
@@ -1,8 +1,36 @@
 <?php
 $arr = [45, 59,237, 599, 498, 91, 890];
 $input_digit = 9;
+$replace_digit = 0;
+$count = 0;
 
+function recDigitParser($digit, $input_digit, $replace_digit) {
+    
+    global $count;
+    
+    if($digit == 0) {
+        return 0;
+    }
+    
+    $res = recDigitParser((int)($digit / 10), $input_digit, $replace_digit);
+    $lastDigit = $digit % 10;
+    
+    $res *= 10;
+    
+    if($lastDigit === $input_digit) {
+      $lastDigit = $replace_digit;  
+      ++$count;
+    }
+    
+    $res += $lastDigit;
+   
+    return $res;
+    
+} 
 
+foreach($arr as $key => $data) {
+    $arr[$key] = recDigitParser($data, $input_digit, $replace_digit);
+}
 
-
-
+print_r($arr);
+echo $count;
