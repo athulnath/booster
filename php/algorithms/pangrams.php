ef01e06b809e582e8ba5070e3a6351a8e49470f8
athulnath <athulnath.t@gmail.com>
2016-07-02 18:49:13 +0530

    update logic

--- php/algorithms/pangrams.php
+++ php/algorithms/pangrams.php
@@ -1,18 +1,22 @@
 <?php 
-$_fp = fopen("php://stdin","r");
-$data = trim(fgets($_fp)); 
-$n = strlen($data);
-$tmp = [];
-for($i = 0; $i < $n; $i++) {
-	$isSpeChar = false;
-	for($j = 0; $j < count($tmp); $j++) {
-		if(strtolower($data[$i]) == strtolower($tmp[$j]) || !((ord($data[$i]) >= 97 && ord($data[$i]) <= 122) || (ord($data[$i]) >= 65 && ord($data[$i]) <= 90)) ) {
-			$isSpeChar = true;
+class Pangram {
+	public function isPangram($string) {
+		for($i = 65; $i <= 90; $i++) {
+			if(stripos($string, chr($i)) === false) {
+				return false;
+			}
 		}
-	}
-
-	if(!$isSpeChar) {
-		$tmp[] = $data[$i];
+		return true;
 	}
 }
-echo count($tmp) === 26 ? "pangram": "not pangram";
+
+
+class Reader {
+	public static function stdin() {
+		$_fp = fopen("php://stdin","r");
+		return trim(fgets($_fp)); 		
+	}
+}
+
+$data = Reader::stdin();
+echo (new Pangram())->isPangram($data) ? "pangram": "not pangram";
